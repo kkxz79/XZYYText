3342661b7ef21a586af289ea7856867e51c334c7
kkxz <kkxz79@126.com>
2018-12-27 18:45:26 +0800

    YYText使用。

---
+++ XZYYText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZYYText
+//
+//  Created by kkxz on 2018/12/27.
+//  Copyright © 2018 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZYYText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZYYText
+//
+//  Created by kkxz on 2018/12/27.
+//  Copyright © 2018 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
